95a5727b23ceb0e1e2df1f69e0518a2f2d5743fc
Andrew Resch <andrewresch@gmail.com>
2011-05-02 16:25:04 +0000

    Remove GPL header from setup.py as its not supposed to be there. See the LICENSE file for information on the license of pyenet.

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-#
-# setup.py
-#
-# Copyright (C) 2010 Andrew Resch <andrewresch@gmail.com>
-#
-# pyenet is free software.
-#
-# You may redistribute it and/or modify it under the terms of the
-# GNU General Public License, as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option)
-# any later version.
-#
-# pyenet is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with deluge.    If not, write to:
-#     The Free Software Foundation, Inc.,
-#     51 Franklin Street, Fifth Floor
-#     Boston, MA  02110-1301, USA.
-#
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
