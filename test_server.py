79c0aa150553c154f1d6e3031c7e1b9333a92461
Way, No <noway@2ch.hk>
2017-01-11 02:48:23 +1300

    return the files back

--- test_server.py
+++ test_server.py
@@ -16,22 +16,20 @@ shutdown_recv = False
 while run:
     # Wait 1 second for an event
     event = host.service(1000)
-    # def f(self, address, data): print(self, address, data)
-
     if event.type == enet.EVENT_TYPE_CONNECT:
-        print("%s: S CONNECT" % event.peer.address)
+        print("%s: CONNECT" % event.peer.address)
         connect_count += 1
     elif event.type == enet.EVENT_TYPE_DISCONNECT:
-        print("%s: S DISCONNECT" % event.peer.address)
+        print("%s: DISCONNECT" % event.peer.address)
         connect_count -= 1
         if connect_count <= 0 and shutdown_recv:
             run = False
     elif event.type == enet.EVENT_TYPE_RECEIVE:
-        print("%s: S IN:  %r" % (event.peer.address, event.packet.data))
+        print("%s: IN:  %r" % (event.peer.address, event.packet.data))
         msg = event.packet.data
         if event.peer.send(0, enet.Packet(msg)) < 0:
-            print("%s: S Error sending echo packet!" % event.peer.address)
+            print("%s: Error sending echo packet!" % event.peer.address)
         else:
-            print("%s: S OUT: %r" % (event.peer.address, msg))
+            print("%s: OUT: %r" % (event.peer.address, msg))
         if event.packet.data == "SHUTDOWN":
             shutdown_recv = True
