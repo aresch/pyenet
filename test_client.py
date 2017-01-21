56b4f8952a87c211f1fe9a879327f46056c4e3f1
Way, No <noway@2ch.hk>
2017-01-22 03:34:19 +1300

    moving disconnect managing into the loop, making tests deterministic and fixing network latency tests bug

--- test_client.py
+++ test_client.py
@@ -1,5 +1,11 @@
 import enet
-import os
+import random
+import sys
+
+try:
+    random.seed(sys.argv[1])
+except IndexError:
+    pass
 
 SHUTDOWN_MSG = b"SHUTDOWN"
 MSG_NUMBER = 10
@@ -20,7 +26,7 @@ while run:
     elif event.type == enet.EVENT_TYPE_RECEIVE:
         print("%s: IN:  %r" % (event.peer.address, event.packet.data))
         continue
-    msg = os.urandom(40)
+    msg = bytes(bytearray([random.randint(0,255) for i in range(40)]))
     packet = enet.Packet(msg)
     peer.send(0, packet)
 
@@ -28,7 +34,7 @@ while run:
     if counter >= MSG_NUMBER:
         msg = SHUTDOWN_MSG
         peer.send(0, enet.Packet(msg))
-        host.service(0)
+        host.service(100)
         peer.disconnect()
 
     print("%s: OUT: %r" % (peer.address, msg))
@@ -36,27 +42,29 @@ while run:
 
 # Part of the test to do with intercept callback and socket.send
 peer = host.connect(enet.Address(b"localhost", 54301), 1)
+shutdown_scheduled = False
+run = True
 
 def receive_callback(address, data):
-    if data != b"\xff\xff\xff\xffstatusResponse\n":
+    global shutdown_scheduled
+
+    if shutdown_scheduled:
+        return
+
+    if data == b"\xff\xff\xff\xffstatusResponse\n":
+        # if the test gets to this point, it means it has passed. Disconnect is a clean up
+        shutdown_scheduled = True
+    else:
         # error messages are not propagating
         # through cython
-        print("data != statusResponse")
+        print("data != statusResponse. Instead of expected, got %r" % data)
         assert(False)
 
-    # if the test gets to this point, it means it has passed. Disconnect is a clean up
-    msg = SHUTDOWN_MSG
-    peer.send(0, enet.Packet(msg))
-    host.service(0)
-    peer.disconnect()
-    host.intercept = None
-
-run = True
 while run:
     event = host.service(1000)
     if event.type == enet.EVENT_TYPE_CONNECT:
         print("%s: CONNECT" % event.peer.address)
-        msg = os.urandom(40)
+        msg = bytes(bytearray([random.randint(0,255) for i in range(40)]))
         packet = enet.Packet(msg)
         peer.send(0, packet)
 
@@ -67,6 +75,13 @@ while run:
         continue
     elif event.type == enet.EVENT_TYPE_RECEIVE:
         print("%s: IN:  %r" % (event.peer.address, event.packet.data))
+        continue
+
+    if shutdown_scheduled:
+        msg = SHUTDOWN_MSG
+        peer.send(0, enet.Packet(msg))
+        host.service(100)
+        peer.disconnect()
         continue
 
     host.socket.send(peer.address, b"\xff\xff\xff\xffgetstatus\x00")
